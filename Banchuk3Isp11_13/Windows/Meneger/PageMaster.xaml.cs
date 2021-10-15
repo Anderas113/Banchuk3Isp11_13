e0dbd50532aba7f15e4fe2458a63f28be5220e79
Anderas113 <a-banchuk@bk.ru>
2021-10-15 12:35:14 +0300

    Добавьте файлы проекта.

---
+++ Banchuk3Isp11_13/Windows/Meneger/PageMaster.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Banchuk3Isp11_13.Windows.Meneger
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMaster.xaml
+    /// </summary>
+    public partial class PageMaster : Page
+    {
+        public PageMaster()
+        {
+            InitializeComponent();
+        }
+    }
+}
