7151e871c3b2fb8a479f3c90e22de08d168f78ef
andreas113 <abanchuk113@gmail.com>
2021-10-26 12:44:09 +0300

    Было добавлено окно для работы с ветиринароми

---
+++ Banchuk3Isp11_13/EF/Entfr.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banchuk3Isp11_13.EF
+{
+    class Entfr
+    {
+        public static VeterenarEntities Context { get; } = new VeterenarEntities();
+    }
+}
